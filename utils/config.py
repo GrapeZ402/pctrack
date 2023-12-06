e3fe3973f0ecf34032b67e6ad193988c60a6b163
GrapeZ402 <150443054+GrapeZ402@users.noreply.github.com>
2023-12-07 00:34:28 +0800

    Add files via upload

--- utils/config.py
+++ utils/config.py
@@ -3,8 +3,8 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data', type=int, choices=[0,1,2,3], default=1)
-parser.add_argument('--delay', type=int, choices=[0,1,2,3,4], default=1)
+parser.add_argument('--data', type=str, choices=['OnRamp','Intersect','Express','UrbMixed'], default='OnRamp')
+parser.add_argument('--delay', type=int, choices=[25,30,35,40,45], default=30)
 parser.add_argument('--method', type=str, choices=['ours', 'base'], default='ours')
 parser.add_argument('--source', type=str, default='datasets/OnRamp/')
 parser.add_argument('--label', type=str, default='datasets/labels/OnRamp_label/')
@@ -19,25 +19,23 @@ source = args.source
 label_path = args.label
 
 
-WINDOW_LIST = [25,30,35,40,45]
 feature_quality_level = 0.2
 downsample = 5
 FPS = 30
 
-if data_param==2:
+if data_param=='Express':
     fw,fh = 1280, 720
 else:
     fw,fh = 1920, 1080
 
 data_n = 0
 
+data_l = {'OnRamp':0, 'Intersect':1, 'Express':2, 'UrbMixed':3}
 weight_path_l = ['weights/Intersect.pth','weights/OnRamp.pth','weights/Express.pth','weights/UrbMixed.pth']
-result_path_l = 'runs/result'
+result_path= 'runs/result'
 
-WINDOW = WINDOW_LIST[delay_param]
-
-weight_path = weight_path_l[data_param]
-result_path = result_path_l + str(data_param+1) + '-' + method + '_' + str(WINDOW) + '/'
+WINDOW = delay_param
+weight_path = weight_path_l[data_l[data_param]]
 
 
 #device
